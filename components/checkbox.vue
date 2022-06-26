d4d5756177a52c560f869d619853b145ee822167
adhitya_tri <adhitya.tri@gramedia.id>
2022-06-26 11:33:10 +0700

    simple checkbox

---
+++ components/checkbox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="checkbox-wrap">
+        <input @change="handleChange(data)" type="checkbox" :value="data"/>
+        <label>{{data}}</label>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        data: [String, Object]
+    },
+    methods: {
+        handleChange(value){
+            this.$emit('input', value)
+        }
+    }
+}
+</script>
+
+<style>
+.checkbox-wrap{
+    border: 1px solid red;
+    margin-bottom: .5em;
+    padding: .5em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,63 @@
 <template>
-  <Tutorial/>
+  <div class="base">
+    <h3>Simple Checkbox</h3>
+
+    <div v-for="(data, index) in items" :key="index">
+      <checkbox :data="data" v-model="selected.select" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data(){
+    const items = ["1","2","3","4"]
+    return {
+      items, 
+      selected: {
+        select: '',
+        active: []
+      }
+    }
+  },
+  watch: {
+    "selected.select"() {
+      this.onCheckbox();
+    }
+  },
+  methods: {
+    onCheckbox(){
+      if (this.selected.active.length == 0){
+        this.selected.active.push(this.selected.select)
+      }else{
+        const index = this.selected.active.indexOf(this.selected.select);
+
+        console.log(index)
+        if (index > -1) {
+          console.log("Hapus")
+          this.selected.active.splice(index, 1);
+        }else{
+          this.selected.active.push(this.selected.select)
+        }
+      }
+    }
+  }
 }
 </script>
+
+<style>
+body, html{
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.base{
+
+}
+</style>
